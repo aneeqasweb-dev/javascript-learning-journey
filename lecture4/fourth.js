c6e95ddacaa18e2b5feb9736162717b66abebc97
aneeqasweb-dev <aneeqa.dev@gmail.com>
2026-07-02 02:47:03 +0500

    Explore JavaScript Arrays

---
+++ lecture4/fourth.js
@@ -0,0 +1,97 @@
+
+// let marks_student1 = 97;
+
+// let marks_student2 = 96;
+
+// let marks =[76,98,89,97,85];
+
+// console.log(marks);
+
+// console.log(marks.length);
+
+// let heroes = ["lee min ho","song joong ki","cha eun woo"];
+
+// // for of
+// for (let hero of heroes){
+//     console.log(hero);
+// }
+
+// console.log(heroes[0]);
+// console.log(heroes[1]);
+
+// for (let idx=0; idx<heroes.length; idx++ ){
+//     console.log(heroes[idx]);
+// }
+
+//IN JS STRINGS ARE IMMUTABLE  //we cann't change them
+//BUT ARRAYS ARE MUTABLE IN JS
+
+//PRACTICE Qs#1
+
+// let marks = [45,98,65,77,59,85];
+
+// let sum =  0;
+
+// for (let val of marks){
+//     sum += val;
+// }
+// let avg = sum / marks.length;
+
+// console.log(`avg marks of class =  ${avg}`);
+
+// Qs#2
+
+// let items = [250, 645, 300, 900, 50];
+
+// let i = 0;
+// for (let val of items){
+//     console.log(`val at index  ${i} = ${val}`);
+//    let offer = val /10;
+//    items[i] = items [i] - offer;
+//    console.log(`value after offer = ${items[i]}`)
+//     i++; 
+// }
+// for (let i =0; i < items.length; i++) {
+
+// }
+//   Q#3
+
+// let foodItems = ["tomato", "apple","orange","lichi"];
+// let marks = [34,89,65,56,98];
+// console.log(foodItems);
+// console.log(foodItems.toString ()) ;
+
+
+// let marvelHeroes =["spiderman","ironman" ,"thor","Dr.strange"];
+
+// console.log(marvelHeroes);
+
+// console.log(marvelHeroes.slice());
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// arr.splice(2,2,101,102);
+
+//Ad element
+// arr.splice
+//             (2,0,101);
+//Delete Element
+
+// arr.splice(3,1);
+
+//Replace Element
+
+// arr.splice(3,1,102);
+
+// PRACTICE QUESTION:
+  
+// let companies = ["Bloomberg","Microsoft","Uber","Netflix","IBM","Google"];
+
+// // companies.shift();
+// companies.splice(2,1,"Ola")
+
+// companies.push("Amazon");
+
+
+
+
