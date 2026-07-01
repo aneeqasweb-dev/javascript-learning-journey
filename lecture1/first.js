cfbf2540356315c9d2da6d7a1f0322b858d0277c
aneeqasweb-dev <aneeqa.dev@gmail.com>
2026-07-01 14:50:33 +0500

    implement examples of variables and datatypes

---
+++ lecture1/first.js
@@ -0,0 +1,59 @@
+
+// name = "Perfect Crown";
+// price = 99.99;
+// x = null;
+// y = undefined;
+// isfollow = true;
+// console.log(isfollow);
+
+// let x = BigInt(890);
+// let y = Symbol("Hello!"); 
+
+// const student = {
+//   fullName: "Aneeqa",
+//   age :18,
+//   grade: "Aplus",
+//   isPass:true
+
+// };
+// student["fullName"]= "Blackpink" ;
+// console.log(student.fullName); 
+
+// PRACTICE QUESTIONS
+ 
+// Q#1
+// const product = {
+//    title :"Ball Pen",
+//    offer : 5,
+//    rating: 4,
+
+// }
+// console.log(product);
+
+// Q#2
+
+// const instaProfile ={
+//   username : "@shradhakhapra",
+//   isFollow : true, 
+//   isMessage :false,
+//   followers :"569k",
+//   following :4,
+//   posts  : 195,
+//   bio : "apnacollege|Ex-Microsoft,DRDO",
+// };
+// console.log( typeof instaProfile.isFollow );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
